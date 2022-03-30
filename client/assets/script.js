149e2e11a4553e57392e51bf0fe0f29ebe0d7f7f
AverKill <DavidRQuigleyWork@yahoo.com>
2022-03-30 17:49:13 +0100

    Not finished, making the pages work

--- client/assets/script.js
+++ client/assets/script.js
@@ -3,11 +3,12 @@ const pList       = document.querySelector('#pList');
 const btnRandom   = document.querySelector('#btnRandom');
 const pRandom     = document.querySelector('#pRandom');
 
-btnRandom.addEventListener('click', () => {
-    fetch('http://localhost:3000/food/random')
-    .then(res => res.json())
-    .then(data => pRandom.textContent = JSON.stringify(data.name))
-});
+
+// btnRandom.addEventListener('click', () => {
+//     // fetch('http://localhost:3000/food/random')
+//     // .then(res => res.json())
+//     // .then(data => pRandom.textContent = JSON.stringify(data.name))
+// });
 btnNameList.addEventListener('click', () => {
     fetch('http://localhost:3000/food/name')
     .then(res => res.json())
